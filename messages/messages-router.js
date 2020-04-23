c1aee029466d5ab53d856113e2808913192ba088
Amber-Pittman <amber@amberpittman.com>
2020-04-23 10:42:38 -0400

    Completed get request

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -18,8 +18,20 @@ router.get("/", async (req, res, next) => {
                 // converted by Knex to the SQL statement.
             // Then it's going to send that statement to our database
             // It's going to get the results it returns and stick it in a variable
-                
-        await db.select("*").from("messages")
+               // The variable is what we assign this command to 
+        const messages = await db.select("*").from("messages")
+            // If everything runs correctly, we should have a list of messages in the
+                // messages variable.
+            // Single or Double quotes don't matter here because this is JS, not SQL
+                // Knex will convert it automatically
+            // Why do we have to rely on promises when talking to our database? Because the
+                // data has to travel. Our database is a completely separate program
+                // from our node program. Since we don't know how long it's going to take
+                // before it reaches its destination, we use a promise. 
+        // 4. Return it to the response
+        // 5. Confirm by going into Insomnia. Create Get Messages request. 
+            // http://localhost:4000/messages
+        res.json(messages);
     } catch(error) {
         next(error)
     }
